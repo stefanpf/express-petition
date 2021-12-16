76893b456e6a6051af6948404e69f2c25492c8b5
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2021-12-16 15:08:56 +0100

    tests for GET methods, not sure why one fails with status 500

---
+++ tests/petition-router.test.js
@@ -0,0 +1,72 @@
+const supertest = require("supertest");
+const { app } = require("../server");
+const cookieSession = require("cookie-session");
+
+test("GET / redirects to /petition when logged in", () => {
+    cookieSession.mockSessionOnce({ userId: 1 });
+    return supertest(app).get("/").expect(302).expect("location", "/petition");
+});
+
+test("GET / redirects to /login when not logged in", () => {
+    return supertest(app).get("/").expect(302).expect("location", "/login");
+});
+
+test("GET /petition redirects to /login when not logged in", () => {
+    return supertest(app)
+        .get("/petition")
+        .expect(302)
+        .expect("location", "/login");
+});
+
+test("GET /petition functional when logged in and hasn't signed", () => {
+    cookieSession.mockSessionOnce({ userId: 1 });
+    return supertest(app).get("/petition").expect(200);
+});
+
+test("GET /petition redirects to /thanks when logged in and has signed", () => {
+    cookieSession.mockSessionOnce({ userId: 1, hasSigned: true });
+    return supertest(app)
+        .get("/petition")
+        .expect(302)
+        .expect("location", "/thanks");
+});
+
+test("GET /thanks functional when logged in and has signed", () => {
+    cookieSession.mockSessionOnce({ userId: 1, hasSigned: true });
+    return supertest(app).get("/thanks").expect(200);
+});
+
+test("GET /thanks redirects to /petition when logged in and hasn't signed", () => {
+    cookieSession.mockSessionOnce({ userId: 1 });
+    return supertest(app)
+        .get("/thanks")
+        .expect(302)
+        .expect("location", "/petition");
+});
+
+test("GET /thanks redirects to /login when not logged in", () => {
+    return supertest(app)
+        .get("/thanks")
+        .expect(302)
+        .expect("location", "/login");
+});
+
+test("GET /signers functional when logged in and has signed", () => {
+    cookieSession.mockSessionOnce({ userId: 1, hasSigned: true });
+    return supertest(app).get("/signers").expect(200);
+});
+
+test("GET /signers redirects to /petition when logged in and hasn't signed", () => {
+    cookieSession.mockSessionOnce({ userId: 1 });
+    return supertest(app)
+        .get("/signers")
+        .expect(302)
+        .expect("location", "/petition");
+});
+
+test("GET /signers redirects to /login when not logged in", () => {
+    return supertest(app)
+        .get("/signers")
+        .expect(302)
+        .expect("location", "/login");
+});
--- tests/profile-router.test.js
+++ tests/profile-router.test.js
@@ -1,1 +1,23 @@
+const supertest = require("supertest");
+const { app } = require("../server");
+const cookieSession = require("cookie-session");
+
 jest.mock("../utils/db.js");
+
+test("GET /profile functional when logged in", () => {
+    cookieSession.mockSessionOnce({ userId: 1 });
+    return supertest(app).get("/profile").expect(200);
+});
+
+test("GET /profile redirects to /login when not logged in", () => {
+    return supertest(app).get("/").expect(302).expect("location", "/login");
+});
+
+test("GET /profile/edit functional when logged in", () => {
+    cookieSession.mockSessionOnce({ userId: 1 });
+    return supertest(app).get("/profile/edit").expect(200);
+});
+
+test("GET /profile/edit redirects to /login when not logged in", () => {
+    return supertest(app).get("/").expect(302).expect("location", "/login");
+});
