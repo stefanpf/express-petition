ab316e3d3e82a422917a5381ce6eca4d0af89aec
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2021-12-18 10:26:56 +0100

    unnecessary function

--- utils/helper-functions.js
+++ utils/helper-functions.js
@@ -9,10 +9,6 @@ function checkValidEmail(str) {
     );
 }
 
-function capitalizeFirstLetter(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
 module.exports = {
     logUrl,
     checkValidEmail,
